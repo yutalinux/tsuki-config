67f7e80f5bb99eeb74fdde8127dd7324c4784537
Yuta <jp.yuta0131@gmail.com>
2024-11-02 10:39:11 +0900

    update: PKGBUILD

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-# Tsuki Config
+# [Tsuki Config](https://github.com/yutalinux/tsuki-config)
 
 [Tsuki Linux](https://github.com/yutalinux/tsuki-linux) の設定ファイルのリポジトリです
--- configs/ja_JP/init.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-echo "ja_JP.UTF-8 UTF-8" >> /etc/locale.gen
-locale-gen
-
-reflector --save /etc/pacman.d/mirrorlist --country Japan --protocol https --latest 5
