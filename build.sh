4fcccdd135e7aee66ba2059ebb0af26b0126e18f
Yuta <jp.yuta0131@gmail.com>
2024-11-02 11:23:01 +0900

    add: build.yml

--- build.sh
+++ build.sh
@@ -1,1 +1,26 @@
 cd packages
+
+# 各サブディレクトリに対して処理を実行
+for dir in */; do
+  if [ -d "$dir" ]; then
+    echo "Processing package in $dir"
+    cd "$dir"
+
+    if [ -f "PKGBUILD" ]; then
+      echo "Building package in $dir"
+      makepkg -sf
+
+      if [ $? -ne 0 ]; then
+        echo "Error building package in $dir"
+      else
+        echo "Successfully built package in $dir"
+      fi
+    else
+      echo "No PKGBUILD found in $dir"
+    fi
+
+    cd ..
+  fi
+done
+
+echo "All packages processed"
